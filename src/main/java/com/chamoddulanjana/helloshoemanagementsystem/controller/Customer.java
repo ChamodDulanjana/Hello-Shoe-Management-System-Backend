06b254aeb25b9b4a211d7260aa91cab518ea7b6d
ChamodDulanjana <chamodperera128@gmail.com>
2024-04-30 22:05:17 +0530

    adding validations...

--- src/main/java/com/chamoddulanjana/helloshoemanagementsystem/controller/Customer.java
+++ src/main/java/com/chamoddulanjana/helloshoemanagementsystem/controller/Customer.java
@@ -3,7 +3,14 @@ package com.chamoddulanjana.helloshoemanagementsystem.controller;
 import com.chamoddulanjana.helloshoemanagementsystem.dto.CustomerDTO;
 import com.chamoddulanjana.helloshoemanagementsystem.service.custom.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.text.ParseException;
 import java.util.List;
@@ -20,17 +27,27 @@ public class Customer {
         return "Customer health check";
     }
 
-    @PostMapping
-    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customer){
-        return customerService.saveCustomer(customer);
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> saveCustomer(@Validated @RequestBody CustomerDTO customer, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getFieldErrors().get(0).getDefaultMessage());
+        }
+        try {
+            customerService.saveCustomer(customer);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Customer saved successfully!");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Internal server error! | Customer saved successfully!  \nMore Details\n"+e.getMessage());
+        }
     }
 
-    @GetMapping("/{code}")
+    @GetMapping(value = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
     public CustomerDTO getCustomerById(@PathVariable String code){
         return customerService.getCustomerById(code);
     }
 
-    @GetMapping("/getAll")
+    @GetMapping(value = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CustomerDTO> getAllCustomer(){
         return customerService.getAllCustomers();
     }
@@ -40,7 +57,7 @@ public class Customer {
         customerService.deleteCustomer(code);
     }
 
-    @PutMapping("/{code}")
+    @PutMapping(value = "/{code}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public void updateCustomer(@RequestBody CustomerDTO customer, @PathVariable String code){
         customerService.updateCustomer(customer, code);
     }
