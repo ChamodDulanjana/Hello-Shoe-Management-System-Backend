ba23a3c998091270cc1ec75a9ab2bffeccb08a76
ChamodDulanjana <chamodperera128@gmail.com>
2024-05-15 15:36:06 +0530

    User api..!

--- src/main/java/com/chamoddulanjana/helloshoemanagementsystem/controller/User.java
+++ src/main/java/com/chamoddulanjana/helloshoemanagementsystem/controller/User.java
@@ -1,6 +1,9 @@
 package com.chamoddulanjana.helloshoemanagementsystem.controller;
 
 import com.chamoddulanjana.helloshoemanagementsystem.dto.UserDTO;
+import com.chamoddulanjana.helloshoemanagementsystem.exception.NotFoundException;
+import com.chamoddulanjana.helloshoemanagementsystem.service.custom.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +14,10 @@ import org.springframework.web.bind.annotation.*;
 
 @Repository
 @RequestMapping("api/v1/user")
+@RequiredArgsConstructor
 public class User {
+
+    private final UserService userService;
 
     @GetMapping("/health")
     public String healthCheck(){
@@ -23,6 +29,21 @@ public class User {
     public ResponseEntity<?> saveUser(@Validated @RequestBody UserDTO user, BindingResult bindingResult){
         if (bindingResult.hasErrors()) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(bindingResult.getFieldErrors().get(0).getDefaultMessage());
+        }
+        try {
+            userService.saveUser(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(" User saved successfully");
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error! | Customer saved successfully!  \\nMore Details\\n\"+e.getMessage()" +e.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getUserById(@PathVariable String code){
+        try {
+            return ResponseEntity.ok(userService.findUserById(code));
+        }catch (NotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found!");
         }
     }
 }
